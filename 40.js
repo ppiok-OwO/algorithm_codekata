26cdc72f8c762a3e6ee20b559166d313d192d127
ppiok-OwO <solcorin@naver.com>
2024-11-19 10:04:20 +0900

    39번까지

---
+++ 40.js
@@ -0,0 +1,13 @@
+// 문제 설명
+// 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+function solution(n) {
+  var answer = 0;
+  // parseInt를 사용하기 위해 n을 문자열로 변환
+
+  n.toString().parseInt(n, 3);
+
+  
+
+  return answer;
+}
